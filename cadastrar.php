37992f2bcc4d4294ace0c3c61d7b94812839cb94
Neto Sales <contato@netosales.com.br>
2017-12-20 17:53:29 -0300

    Arquivos da aula 92, apos acerto do erro.

--- cadastrar.php
+++ cadastrar.php
@@ -3,7 +3,7 @@ $email = $_POST["inputEmail"];
 //var_dump($email);
 //var_dump($_POST);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, " https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
@@ -13,14 +13,14 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 		"remoteip"=>$_SERVER["REMOTE_ADDR"]
 	)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$recaptcha = json_encode(curl_exec($ch), true);
+$recaptcha = json_decode(curl_exec($ch), true);
 curl_close($ch);
 //var_dump($recaptcha);
 
-if ($recaptcha["sucess"] === true){
-	echo "OK: ".$_POST["inputEmail"];
+if ($recaptcha["success"] === true){
+     echo "OK: ".$_POST["inputEmail"];
 }else {
-	header("Location: exemplo-04.php");
+     header("Location: exemplo-04.php");
 }
 
 ?>
